1b100fbaf5ea41efa2c72787c82e383e9df54fb0
virsavik <dhuuloc8818@gmail.com>
2022-09-21 17:22:12 +0700

    Update project

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment, incrementByAmount, incrementAsync, incrementIfOdd, selectCount } from './counterSlice';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
+} from './counterSlice';
 import styles from './Counter.module.css';
 
 export default function Counter() {
@@ -38,13 +45,25 @@ export default function Counter() {
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-        <button type="button" className={styles.button} onClick={() => dispatch(incrementByAmount(incrementValue))}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => dispatch(incrementByAmount(incrementValue))}
+        >
           Add Amount
         </button>
-        <button type="button" className={styles.asyncButton} onClick={() => dispatch(incrementAsync(incrementValue))}>
+        <button
+          type="button"
+          className={styles.asyncButton}
+          onClick={() => dispatch(incrementAsync(incrementValue))}
+        >
           Add Async
         </button>
-        <button type="button" className={styles.button} onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+        >
           Add If Odd
         </button>
       </div>
